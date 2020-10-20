18201e8f3a37a5e0c00abc7a0096990dac4a2e54
Jack Kilgallen <43824116+JKilgallen@users.noreply.github.com>
2020-10-20 17:33:18 +0100

    Changed answers from function calls to variables

--- Chapter2.scm
+++ Chapter2.scm
@@ -1,23 +1,23 @@
-(define (get-answer-1.1 )
+(define answer-1.1 
     (* (+ 1 39) (- 53 45)))
 
-(define (get-answer-1.2)
+(define answer-1.2
     (+ (/ 1020 39) (* 45 2)))
 
-(define (get-answer-1.3)
+(define answer-1.3
     (+ 39 48 72 23 91))
 
-(define (get-answer-1.4)
+(define answer-1.4
     (exact->inexact (/ (get-answer-3) 5)))
 
-(define (get-answer-2.1)
+(define answer-2.1
     (atan 0 -1))
 
-(define (get-answer-2.2)
+(define answer-2.2
     (exp (/ 2 3)))
 
-(define (get-answer-2.3)
+(define answer-2.3
     (expt 3 4))
 
-(define (get-answer-2.4)
+(define answer-2.4
     (log 1000))
--- Chapter3.scm
+++ Chapter3.scm
@@ -1,21 +1,35 @@
-(define (get-answer-1.1)
+(define answer-1.1
     (cons "hi" "everybody"))
 
-(define (get-answer-1.2)
+(define answer-1.2
     (cons 0 '()))
 
-(define (get-answer-1.3)
+(define answer-1.3
     (cons 1 (cons 10 100)))
 
-(define (get-answer-1.4)
+(define answer-1.4
     (cons 1 (cons 10 (cons 100 '()))))
 
-(define (get-answer-1.5)
+(define answer-1.5
     (cons #\I (cons "saw" (cons 3 (cons "girls" '())))))
 
-(define (get-answer-1.6)
+(define answer-1.6
     (cons "Sum of" (cons '(1 2 3 4) (cons "is" (cons 10 '())))))
 
+(define answer-2.1
+    0)
+
+(define answer-2.2
+    null)
+
+(define answer-2.3
+    '(1 2 3))
+
+(define answer-2.4
+    (cons 2 (cons 3 4)))
+
+(define answer-2.5
+    (cons 2 (cons 1 '())))
 ;;; The following are the answers to Exercise 2
 ;;;
 ;;; 1: 0
